6ea7d4f3e246dbe55f50eee9ae2dd0c3b54cb792
vishab singh <vishab.singh@tavant.com>
2019-06-26 13:14:27 +0530

    Remove Unsed Code

--- src/test/java/com/vn/react/test/SpringCodacyTests.java
+++ src/test/java/com/vn/react/test/SpringCodacyTests.java
@@ -21,8 +21,6 @@ import com.vn.react.modal.Blog;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static org.springframework.data.mongodb.core.aggregation.ArrayOperators.arrayOf;
-
 
 @RunWith(SpringRunner.class)
 @SpringBootTest( webEnvironment=  SpringBootTest.WebEnvironment.RANDOM_PORT)
