570e07399e1c4e7b23d1864c3d6a8c7db099aa62
vishab singh <vishab.singh@tavant.com>
2019-06-27 16:12:10 +0530

    Coverage ANd Duplicate Code with Methods

--- src/main/java/com/vn/react/duplicatecode/CheckCoverageData.java
+++ src/main/java/com/vn/react/duplicatecode/CheckCoverageData.java
@@ -2,6 +2,8 @@ package com.vn.react.duplicatecode;
 
 public class CheckCoverageData {
 
+    static Node head;
+
     public Node reverse(Node node) {
         Node prev = null;
         Node current = node;
@@ -16,7 +18,47 @@ public class CheckCoverageData {
         return node;
     }
 
-   public  void printList(Node node) {
+    Node reverseUtil(Node curr, Node prev) {
+        if (curr.next == null) {
+            head = curr;
+            curr.next = prev;
+            return head;
+        }
+
+        Node next1 = curr.next;
+        curr.next = prev;
+        reverseUtil(next1, curr);
+        return head;
+    }
+
+    public Node reverseDuplicate(Node node) {
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        node = prev;
+        return node;
+    }
+
+    Node reverseUtilDuplicate(Node curr, Node prev) {
+        if (curr.next == null) {
+            head = curr;
+            curr.next = prev;
+            return head;
+        }
+
+        Node next1 = curr.next;
+        curr.next = prev;
+         reverseUtilDuplicate(next1, curr);
+        return head;
+    }
+
+    public  void printList(Node node) {
         while (node != null) {
             System.out.print(node.data + " ");
             node = node.next;
