471a0093beb6bc943b4536bf81a47d63d029d10c
vishab singh <vishab.singh@tavant.com>
2019-06-27 13:40:51 +0530

    Duplicte Code

--- src/main/java/com/vn/react/duplicatecode/DuplicateCodeClass.java
+++ src/main/java/com/vn/react/duplicatecode/DuplicateCodeClass.java
@@ -24,5 +24,17 @@ public class DuplicateCodeClass {
         }
         return fibonacci(number - 1) + fibonacci(number - 2);
     }
+    int fibonacci1(int number) {
+        if ( number <= 1) {
+            return number;
+        }
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
+    int fibonacci2(int number) {
+        if ( number <= 1) {
+            return number;
+        }
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
 
 }
