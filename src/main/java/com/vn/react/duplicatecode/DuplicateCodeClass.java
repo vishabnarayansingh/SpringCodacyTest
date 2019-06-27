b4aa914379217d5ac1e33d9d4cf388f604984bbd
vishab singh <vishab.singh@tavant.com>
2019-06-27 13:37:24 +0530

    Recursion Function With Duplicate Code

---
+++ src/main/java/com/vn/react/duplicatecode/DuplicateCodeClass.java
@@ -0,0 +1,28 @@
+package com.vn.react.duplicatecode;
+
+public class RecursionFunction {
+
+    int factorial(int number){
+        if(number < 1){
+            return 1;
+        }else{
+            return number * factorial(number -1);
+        }
+    }
+
+    void function(int number) {
+        if(number == 0)
+            return;
+
+        function(number/2);
+        System.out.print("%d" + number);
+    }
+
+    int fibonacci(int number) {
+        if ( number <= 1) {
+            return number;
+        }
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
+
+}
