e79b58a992a74d90a2df671529ef63cdd4190712
vishab singh <vishab.singh@tavant.com>
2019-06-27 14:05:38 +0530

    Added

--- src/test/java/com/vn/react/test/CheckTest.java
+++ src/test/java/com/vn/react/test/CheckTest.java
@@ -1,4 +1,14 @@
 package com.vn.react.test;
 
+import com.vn.react.duplicatecode.CheckCodeDuplication;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CheckTest {
+
+    @Test
+    public void testFib(){
+        CheckCodeDuplication test = new CheckCodeDuplication();
+        Assert.assertEquals(test.fibonacci4(3),2);
+    }
 }
