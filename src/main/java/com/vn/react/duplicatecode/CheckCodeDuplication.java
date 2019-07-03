b40c1022664b7694e23b10205014ff127fd56caa
vishab.singh <vishab.singh@tavant.com>
2019-07-03 17:49:47 +0530

    Update Duplicate Code

--- src/main/java/com/vn/react/duplicatecode/CheckCodeDuplication.java
+++ src/main/java/com/vn/react/duplicatecode/CheckCodeDuplication.java
@@ -18,12 +18,14 @@ public class CheckCodeDuplication {
         System.out.print("%d" + number);
     }
 
+
     int fibonacci(int number) {
         if ( number <= 1) {
             return number;
         }
         return fibonacci(number - 1) + fibonacci(number - 2);
     }
+
 
 
 }
