d42f71964adeafcdd2bb3a90ddd55608ab26d0ad
vishab.singh <vishab.singh@tavant.com>
2019-07-05 18:32:38 +0530

    #DP

--- src/main/java/com/vn/react/duplicatecode/CheckCodeDuplication.java
+++ src/main/java/com/vn/react/duplicatecode/CheckCodeDuplication.java
@@ -120,6 +120,125 @@ public class CheckCodeDuplication {
         int average_b = sum_b / 4;
 
     }
+    public void showDuplicate101() {
+
+        int sum_a = 0;
+
+        for (int i = 0; i < 4; i++)
+            sum_a += array_a[i];
+
+        int average_a = sum_a / 4;
+
+        int sum_b = 0;
+        for (int i = 0; i < 4; i++)
+            sum_b += array_b[i];
+
+        int average_b = sum_b / 4;
+
+    }
+
+
+
+    public void showDuplicate102() {
+
+        int sum_a = 0;
+
+        for (int i = 0; i < 4; i++)
+            sum_a += array_a[i];
+
+        int average_a = sum_a / 4;
+
+        int sum_b = 0;
+        for (int i = 0; i < 4; i++)
+            sum_b += array_b[i];
+
+        int average_b = sum_b / 4;
+
+    }
+    public void showDuplicate103() {
+
+        int sum_a = 0;
+
+        for (int i = 0; i < 4; i++)
+            sum_a += array_a[i];
+
+        int average_a = sum_a / 4;
+
+        int sum_b = 0;
+        for (int i = 0; i < 4; i++)
+            sum_b += array_b[i];
+
+        int average_b = sum_b / 4;
+
+    }
+	
+    public void showDuplicate104() {
+
+        int sum_a = 0;
+
+        for (int i = 0; i < 4; i++)
+            sum_a += array_a[i];
+
+        int average_a = sum_a / 4;
+
+        int sum_b = 0;
+        for (int i = 0; i < 4; i++)
+            sum_b += array_b[i];
+
+        int average_b = sum_b / 4;
+
+    }
+
+    public void showDuplicate105() {
+
+        int sum_a = 0;
+
+        for (int i = 0; i < 4; i++)
+            sum_a += array_a[i];
+
+        int average_a = sum_a / 4;
+
+        int sum_b = 0;
+        for (int i = 0; i < 4; i++)
+            sum_b += array_b[i];
+
+        int average_b = sum_b / 4;
+
+    }
+ public void showDuplicate106() {
+
+        int sum_a = 0;
+
+        for (int i = 0; i < 4; i++)
+            sum_a += array_a[i];
+
+        int average_a = sum_a / 4;
+
+        int sum_b = 0;
+        for (int i = 0; i < 4; i++)
+            sum_b += array_b[i];
+
+        int average_b = sum_b / 4;
+
+    }
+
+    public void showDuplicate107() {
+
+        int sum_a = 0;
+
+        for (int i = 0; i < 4; i++)
+            sum_a += array_a[i];
+
+        int average_a = sum_a / 4;
+
+        int sum_b = 0;
+        for (int i = 0; i < 4; i++)
+            sum_b += array_b[i];
+
+        int average_b = sum_b / 4;
+
+    }
+
 
 
 
